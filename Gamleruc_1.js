e3819514d52727aac17ce53818130e47cbdb97fe
Aniket-123-bit <anisunny782@gmail.com>
2022-04-26 15:58:04 +0530

    [Aniket] Added code all gambling

--- Gamleruc_1.js
+++ Gamleruc_1.js
@@ -2,3 +2,14 @@
 console.log("-------Welcome to Gambling Simulation--------");
 const EVERY_DAY_STAKE = 100;
 const EVERY_DAY_BET = 1;
+console.log("Enter the gambler position");
+    let checkVal = Math.floor(Math.random() * 10) % 3;
+
+        if (checkVal > 0.5)
+        {
+            console.log("The Gambler wins the bet.");
+        }
+        else if (checkVal <= 0.5)
+        {
+            console.log("The Gambler loses the bet.");
+        }
